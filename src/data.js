326b12aa9b41aed357b35ba92dc1b1201b15af98
Akash-75 <akashpandey53358@gmail.com>
2023-09-10 12:15:04 +0530

    Added intial files

---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        id:1,
+        name: "Agra",
+        info: "Agra is a city offering a discovery of the beautiful era. Agra has a rich history, reflected in its numerous monuments dotted in and around the city. The earliest citation for Agra comes from the mythological era, where the epic Mahabharata refer Agra as ‘Agravana’ meaning paradise in Sanskrit. ‘Ptolemy’, the famous second century A.D. geographer, was the first person who referred Agra with its modern name. The Modern Agra was founded by Sikandar Lodi, ruler of Lodi dynasty in 16th century. ",
+        image: "https://tse1.mm.bing.net/th?id=OIP.XM9dxxx9zBi39Xg7uhrcigHaFj&pid=Api&rs=1&c=1&qlt=95&w=143&h=107",
+        price:"35,758",
+    },
+    {
+        id: 2,
+        name:"Jaipur",
+        info:"Jaipur, also known as the pink city, is one of the most beautiful and culturally rich cities in India. It was founded by Maharaja Sawai Jai Singh II in November 1727 and also got its name from this brave Hindu ruler. Located in Northern India, Jaipur is the capital of Rajasthan and also the largest city in this state.",
+        image: "https://static2.tripoto.com/media/transfer/img/OgData/1511856965_ravi_shekhar_365763_fotor.jpg",
+        price: "82,560",
+    },
+    {
+        id: 3,
+        name:"Goa",
+        info:"Goa, state of India, comprising a mainland district on the country’s southwestern coast and an offshore island. It is located about 250 miles (400 km) south of Mumbai (Bombay). One of India’s smallest states, it is bounded by the states of Maharashtra on the north and Karnataka on the east and south and by the Arabian Sea on the west.",
+        image: "https://static2.tripoto.com/media/transfer/img/OgData/1526889683_goa_tour_packages_from_dubai_2_fotor.jpg",
+        price: "29,695",
+    },
+    {
+        id: 4,
+        name:"Varansi",
+        info:"Goa, state of India, comprising a mainland district on the country’s southwestern coast and an offshore island. It is located about 250 miles (400 km) south of Mumbai (Bombay). One of India’s smallest states, it is bounded by the states of Maharashtra on the north and Karnataka on the east and south and by the Arabian Sea on the west.",
+        image: "https://tse1.mm.bing.net/th?id=OIP.LIB385Eg7a9uRoHazIwwBAHaE7&pid=Api&P=0&h=180",
+        price: "31,095",
+    },
+    {
+        id: 5,
+        name:"Darjeeling",
+        info:"The queen of the hills is a Himalayan city in the Indian state of West Bengal. It is internationally renowned as a tourist destination for its spectacular view of Mt.Kanchandzonga, along with its tea industry and the Darjeeling Himalayan Railway, a UNESCO World Heritage site.",
+        image: "https://tse2.mm.bing.net/th?id=OIP.sQXmeEoLyzv8QPveRerMugHaE8&pid=Api&P=0&h=180",
+        price: "78,595",
+    },
+    {
+        id: 6,
+        name:"Jaisalmer",
+        info:"The city of Jaisalmer also acts as the guard to western Rajasthan (and India's) frontier. This 'Golden City' is located close to the Pakistan border and in close proximity to the Thar Desert. The city's most prominent landmark is the Jaisalmer Fort, also called Sonar Qila (Golden Fort).",
+        image: "https://tse4.mm.bing.net/th?id=OIP.zOeDkkHhftgOJ5kEWQkcbQHaFH&pid=Api&P=0&h=180",
+        price: "68,595",
+    },
+    {
+        id: 7,
+        name:"Kochi",
+        info:"Kochi is the only city in the country to have water metro system along with the metro rail. Kochi is known as the financial, commercial and industrial capital of Kerala. It has the highest GDP as well as the highest GDP per capita in the state.",
+        image: "https://tse2.mm.bing.net/th?id=OIP.CnhH2zSNnUm53IhgcEreHAHaEM&pid=Api&P=0&h=180",
+        price: "69,595",
+    },
+
+];
+
+export default data;
